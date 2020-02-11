f297b535325ffd8805ecf362d9951a4481d76cea
Jan <jan.bogoryja@gmail.com>
2020-02-11 21:50:03 +0100

    Expand on the event handling
    Create functions for different event responses

--- src/gitfetcher.py
+++ src/gitfetcher.py
@@ -1,31 +1,59 @@
 import os
 from slack import RTMClient
 
+# region Event handling
+
 
 @RTMClient.run_on(event="message")
 def receiveEvent(**payload):
     data = payload['data']
     web_client = payload['web_client']
-    if '!Hello' in data['text']:
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
-
-        web_client.chat_postMessage(
-          channel=channel_id,
-          text=f"Hi <@{user}>!"
-        )
-        print("Replied to <Hello>")
+    if '@UTU20FNKX' in data['text']:
+        respondMention(data, web_client)
     elif '!data' in data['text']:
-        channel_id = data['channel']
-        thread_ts = data['ts']
-        user = data['user']
+        respondData(data, web_client)
+    elif '!repeat' in data['text']:
+        respondRepeat(data, web_client)
 
-        web_client.chat_postMessage(
-          channel=channel_id,
-          text=f"Here is the data you've requested"
-        )
-        print("Replied to <data>")
+
+def respondData(data, web_client):
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+
+    web_client.chat_postMessage(
+        channel=channel_id,
+        text="Here is the data you've requested"
+    )
+    print("Replied to <data>")
+
+
+def respondMention(data, web_client):
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+
+    web_client.chat_postMessage(
+      channel=channel_id,
+      text=f"Hey, thanks for mentioning me"
+    )
+    print("Replied to <Hello>")
+
+
+def respondRepeat(data, web_client):
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+
+    web_client.chat_postMessage(
+      channel=channel_id,
+      text=data['text'].replace("!repeat", '')
+    )
+    print("Replied to <repeat>")
+
+# endregion
+
+# Slack Connection
 
 
 slack_token = os.environ["GITFETCHER_SLACK_BOT_TOKEN"]
@@ -35,9 +63,10 @@ rtm_client = RTMClient(
     connect_method='rtm.start'
 )
 
-print ("starting listening")
-while(True):
+print("starting listening")
+while True:
     try:
+        print("Connection Started")
         rtm_client.start()
     except KeyError as e:
         print("Error: " + str(e))
